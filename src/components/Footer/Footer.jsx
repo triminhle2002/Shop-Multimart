00856ec5a821ce7dc9c98f30d4dd3c6fa9adf6bd
triminhle2002 <97251680+triminhle2002@users.noreply.github.com>
2023-02-23 13:47:57 +0700

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+// import logo from "../../assets/images/eco-logo.png";
+import "./footer.css";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="logo">
+              {/* <img src={logo} alt="logo" /> */}
+              <div>
+                <h1 className="text-white">Multimart</h1>
+              </div>
+            </div>
+
+            <p className="footer__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+              dolor labore eveniet vitae nulla itaque soluta sit provident con
+              sectetur laboriosam
+            </p>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Top Categories</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#" k>
+                    Mobile Phones
+                  </Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#" k>
+                    Modern Sofa
+                  </Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#" k>
+                    Modern Chair
+                  </Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#" k>
+                    Smart Watchs
+                  </Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="2">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Useful Links</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/shop" k>
+                    Shop
+                  </Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/cart" k>
+                    Cart
+                  </Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/login" k>
+                    Login
+                  </Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#" k>
+                    Privacy Policy
+                  </Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Contact</h4>
+              <ListGroup className="footer__contact">
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-line"></i>
+                  </span>
+                  <p>123 Phuoc Ly, Hoa Minh, Lien Chieu, Da Nang</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-phone-line"></i>
+                  </span>
+                  <p>+0395967905</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line"></i>
+                  </span>
+                  <p>leminhtri2002@gmail.com</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="12">
+            <p className="footer__copyright">
+              Copyright {year} devoloped by Tri Minh Le. All rights reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import "../styles/home.css";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import products from "../assets/data/products";
+
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img.png";
+
+import Services from "../services/Services";
+import ProductsList from "../components/UI/ProductsList";
+
+import Clock from "../components/UI/Clock";
+
+import counterImg from "../assets/images/counter-timer-img.png";
+
+const Home = () => {
+  const [trendingProduct, setTrendingProduct] = useState([]);
+  const [bestSalesProduct, setBestSalesProduct] = useState([]);
+  const [mobileProducts, setMobileProduct] = useState([]);
+  const [wirelessProducts, setWirelessProduct] = useState([]);
+  const [popularProducts, setPopularProduct] = useState([]);
+  const year = new Date().getFullYear();
+
+  useEffect(() => {
+    const filteredTrendingProducts = products.filter(
+      (item) => item.category === "chair"
+    );
+
+    const filteredBestSalesProducts = products.filter(
+      (item) => item.category === "sofa"
+    );
+
+    const filteredMobileProducts = products.filter(
+      (item) => item.category === "mobile"
+    );
+
+    const filteredWirelessProducts = products.filter(
+      (item) => item.category === "wireless"
+    );
+
+    const filteredPopularProducts = products.filter(
+      (item) => item.category === "watch"
+    );
+
+    setTrendingProduct(filteredTrendingProducts);
+    setBestSalesProduct(filteredBestSalesProducts);
+    setMobileProduct(filteredMobileProducts);
+    setWirelessProduct(filteredWirelessProducts);
+    setPopularProduct(filteredPopularProducts);
+  }, []);
+
+  return (
+    <Helmet title={"Home"}>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <p className="hero_subtitle">Trending product in {year}</p>
+                <h2>Make your interior more minimalist and modern</h2>
+                <p>
+                  The company itself is a very successful company. Let him not
+                  seek repulsion where they and other parts of the body, they do
+                  not know the easy way to escape
+                </p>
+
+                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                  <Link to="/shop">SHOP NOW</Link>
+                </motion.button>
+              </div>
+            </Col>
+
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <Services />
+      <section className="trending__products">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title">Trending products</h2>
+            </Col>
+            <Row lg="4">
+              <ProductsList data={trendingProduct} />
+            </Row>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="best__sales">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title">Best sales</h2>
+            </Col>
+            <Row lg="4">
+              <ProductsList data={bestSalesProduct} />
+            </Row>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="timer__count">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="clock__top-content">
+                <h4 className="text-white fs-6">Limited Offes</h4>
+                <h3 className="text-white fs-5 mb-3">Quality Armchair</h3>
+              </div>
+              <Clock />
+
+              <motion.button
+                whileTap={{ scale: 1.2 }}
+                className="buy__btn store_btn"
+              >
+                <Link to="/shop">Visit Store</Link>
+              </motion.button>
+            </Col>
+
+            <Col lg="6" md="6" className="text-end">
+              <img src={counterImg} alt="" />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="new__arrivals">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center mb-5">
+              <h2 className="section__title">New Arrivals</h2>
+            </Col>
+            <Row lg="4">
+              <ProductsList data={mobileProducts} />
+              <ProductsList data={wirelessProducts} />
+            </Row>
+          </Row>
+        </Container>
+      </section>
+      <section className="popular__category">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center mb-5">
+              <h2 className="section__title">Popular in Category</h2>
+            </Col>
+            <Row lg="4">
+              <ProductsList data={popularProducts} />
+            </Row>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Home;
